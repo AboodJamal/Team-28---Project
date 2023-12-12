d5b6665a56022a61946b6e65eccb81724f980ee5
nosyba <nosybamostafa2003@gmail.com>
2023-12-12 22:13:29 +0200

    add global

---
+++ src/nosyba copmonent/globalapp.js
@@ -0,0 +1,19 @@
+import Global  from './globalpage2';
+import './App.css';
+import imageGlobal from "./globalpage.jpg"
+import imggirle from "./boy&girle.png"
+
+function App() {
+  return (
+    <div >
+      <br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      < img src= {imageGlobal} className='imgglobal'/>
+      <Global/>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={imggirle} className='imggirle-boy'  />
+  &nbsp; <span className='phto' >are attending </span>
+ <br></br>
+    </div>
+  );
+}
+
+export default App;
