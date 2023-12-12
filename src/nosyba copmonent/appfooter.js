940464b800611b0bc7912b25aed6d7396a7f836f
nosyba <nosybamostafa2003@gmail.com>
2023-12-12 20:50:32 +0200

    add footer page 1

---
+++ src/nosyba copmonent/appfooter.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+ 
+import Footer from "./nosyba component/footerPage1";
+ 
+import "./App.css";
+ 
+const Appfooter = () => (
+   
+        
+      
+        <Footer />
+        
+  
+);
+ 
+export default Appfooter;
+
