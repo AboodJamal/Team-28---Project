d42cd2fd4443f85096eb711139f3aa7989d4b5e0
Ahmad Baker <148720606+AhmadBakerr@users.noreply.github.com>
2023-12-13 01:25:43 +0200

    Help page articles

---
+++ src/App.js
@@ -0,0 +1,84 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+import Article from './Article';
+import { db } from './firebaseConfig'; // Adjust this import path
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [lastVisible, setLastVisible] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const loadArticles = async () => {
+    setLoading(true);
+    const querySnapshot = await db.collection("articles")
+      .orderBy("timestamp", "desc")
+      .limit(4) // Initial load limit
+      .get();
+
+    const lastVisibleDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
+    setLastVisible(lastVisibleDoc);
+
+    const loadedArticles = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+    setArticles(loadedArticles);
+    setLoading(false);
+  };
+
+  const loadMoreArticles = async () => {
+    if (lastVisible && !loading) {
+      setLoading(true);
+      const nextQuerySnapshot = await db.collection("articles")
+        .orderBy("timestamp", "desc")
+        .startAfter(lastVisible)
+        .limit(2) // Load one more article
+        .get(0);
+
+      const newLastVisible = nextQuerySnapshot.docs[nextQuerySnapshot.docs.length - 1];
+      setLastVisible(newLastVisible);
+
+      const newArticles = nextQuerySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setArticles(prev => [...prev, ...newArticles]);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadArticles();
+  }, []);
+
+  return (
+    <div className="container mt-5">
+      <h2 className="title-page1">Popular Articles</h2>
+      <div className="list-group" id="articles-list">
+        {articles.map(article => (
+      <Article
+      key={article.id}
+      id={article.id}
+      title={article.title}
+      summary={article.summary}
+      initialLikes={article.initialLikes || 0}
+      initialDislikes={article.initialDislikes || 0}
+    />
+  ))}
+      </div>
+      {loading ? (
+        <p>Loading more articles...</p>
+      ) : (
+        <div className="centered-container">
+          <button type="button" className="btn btn-load-more" onClick={loadMoreArticles}>
+            Load more
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
