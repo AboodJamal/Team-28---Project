0b495e21766e698e84baf5eba07d562be6aeee32
nosyba <nosybamostafa2003@gmail.com>
2023-12-13 13:35:14 +0200

    edit

--- src/nosyba copmonent/globalapp.js
+++ src/nosyba copmonent/globalapp.js
@@ -1,7 +1,9 @@
 import Global  from './globalpage2';
-import './App.css';
+import "./nosyba component/global.css"
 import imageGlobal from "./globalpage.jpg"
-import imggirle from "./boy&girle.png"
+import imggirle from "./girleimg.jpeg"
+import imgboy from "./boyimg.jpg"
+import img from "./+34 img.png"
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
       <br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       < img src= {imageGlobal} className='imgglobal'/>
       <Global/>
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={imggirle} className='imggirle-boy'  />
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={imggirle} className='imggirle'  /><img src={imgboy} className='imgboy'  /><img src={img} className='img34'  />
   &nbsp; <span className='phto' >are attending </span>
  <br></br>
     </div>
---
+++ src/nosyba copmonent/globalpage2.js
@@ -0,0 +1,21 @@
+
+const Global =()=>{
+    return(
+
+<div>
+ 
+   
+    <div className="card-body">
+   <h5 className="card-title"><b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Global conference </b></h5>
+   <p className="card-text"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;He moonlights difficult engrossed it, sportsmen. Interested has</p>
+   <p className="card-global">  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;all Devonshire difficult gay assistance joy.</p> <br></br>
+      
+    </div>
+  </div>
+  
+    
+  
+
+    );
+}
+export  default Global; 
