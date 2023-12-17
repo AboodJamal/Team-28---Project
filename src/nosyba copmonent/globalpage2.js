5a3b808ac4fa8c042bcce19bf3de726b2cac0dc8
nosyba <nosybamostafa2003@gmail.com>
2023-12-17 19:46:23 +0200

    edit global

--- src/nosyba copmonent/globalpage2.js
+++ src/nosyba copmonent/globalpage2.js
@@ -1,21 +1,26 @@
 
 const Global =()=>{
-    return(
+  return(
+
 
 <div>
+
+<div className="mb-3">
+  
+<div className="card-body">
+ <h5 className="card-title"><b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Global conference </b></h5>
  
-   
-    <div className="card-body">
-   <h5 className="card-title"><b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Global conference </b></h5>
-   <p className="card-text"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;He moonlights difficult engrossed it, sportsmen. Interested has</p>
-   <p className="card-global">  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;all Devonshire difficult gay assistance joy.</p> <br></br>
-      
+ <p className="card-text"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;He moonlights difficult engrossed it,sportsmen.Interested has</p>
+ <p className="card-global">  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;all Devonshire difficult gay assistance joy.</p> <br></br>
     </div>
   </div>
-  
-    
+
+  </div>
   
 
-    );
+  
+
+
+  );
 }
 export  default Global; 
