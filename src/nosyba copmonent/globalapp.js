39474eba7e6244ecddafa2e3606ba81b429d7809
nosyba <nosybamostafa2003@gmail.com>
2023-12-18 16:29:56 +0200

    I edit glbal

--- src/nosyba copmonent/globalapp.js
+++ src/nosyba copmonent/globalapp.js
@@ -1,23 +1,13 @@
 import Global  from './globalpage2';
 import "./nosyba component/App.css";
-import imageGlobal from "./globalpage.jpg"
-import imggirle from "./girleimg.jpeg"
-import imgboy from "./boyimg.jpg"
-import img from "./+34 img.png"
 
 function App() {
   return (
     <div >
-      <br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      
-
-
-    <img src={imageGlobal} className="img-fluid" alt='' />
+     
 
       <Global/>
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={imggirle} className='imggirle'  /><img src={imgboy} className='imgboy'  /><img src={img} className='img34'  />
-  &nbsp; <span className='phto' >are attending </span>
- <br></br>
+ 
     </div>
    
   );
--- src/nosyba copmonent/globalpage2.js
+++ src/nosyba copmonent/globalpage2.js
@@ -1,26 +1,40 @@
 
+
+import imggirle from "./girleimg.jpeg"
+import imgboy from "./boyimg.jpg"
+import img from "./+34 img.png"
+import imageGlobal from "./globalpage.jpg"
+
 const Global =()=>{
-  return(
-
-
+    return(
 <div>
+        <br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-<div className="mb-3">
-  
-<div className="card-body">
- <h5 className="card-title"><b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Global conference </b></h5>
+<img src={imageGlobal} className="img-fluid" alt='' />
  
- <p className="card-text"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;He moonlights difficult engrossed it,sportsmen.Interested has</p>
- <p className="card-global">  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;all Devonshire difficult gay assistance joy.</p> <br></br>
+<div className="mb-3">
+    
+<div className="card-body">
+   <h5 className="card-title"><b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Global conference </b></h5>
+   
+   <p className="card-text"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;He moonlights difficult engrossed it,sportsmen.Interested has</p>
+   <p className="card-global">  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;all Devonshire difficult gay assistance joy.</p> <br></br>
+     
+     
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={imggirle} className='imggirle'  /><img src={imgboy} className='imgboy'  /><img src={img} className='img34'  />
+  &nbsp; <span className='phto' >are attending </span>
+ <br></br>
     </div>
-  </div>
+   
+      </div>
+    </div>
+ 
+   
+    
 
-  </div>
+    
   
 
-  
-
-
-  );
+    );
 }
 export  default Global; 
